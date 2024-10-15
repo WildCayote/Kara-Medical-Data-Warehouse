45c2c9c11759af16d4c42cf1cf5aa388379c27f3
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-10-15 10:22:23 +0300

    Feat: defined static methods of the class and implemented the remove_emojis method

---
+++ scripts/data_cleaner.py
@@ -0,0 +1,46 @@
+import re
+
+class Preprocessor:
+    """
+    A class which houses methods that involve preprocessing Amharic text.
+    """
+
+    @staticmethod
+    def remove_emojis(text: str):
+        """
+        A function that removes emojis found in a text.
+
+        Args:
+            text(str): the string potentially containing emojis
+        Returns:
+            The string without any emojis
+        """
+        emoji_pattern = re.compile(
+            "[" 
+            "\U0001F600-\U0001F64F"  # emoticons
+            "\U0001F300-\U0001F5FF"  # symbols & pictographs
+            "\U0001F680-\U0001F6FF"  # transport & map symbols
+            "\U0001F700-\U0001F77F"  # alchemical symbols
+            "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+            "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+            "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+            "\U0001FA00-\U0001FA6F"  # Chess Symbols
+            "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+            "\U00002702-\U000027B0"  # Dingbats
+            "\U000024C2-\U0001F251" 
+            "]+", 
+            flags=re.UNICODE
+        )
+        return emoji_pattern.sub(r'', text)
+
+    @staticmethod
+    def remove_special_characters(text: str):
+        """"""
+    
+    @staticmethod
+    def normalize_data(text: str):
+        """"""
+    
+    @staticmethod
+    def remove_extra_space(text: str):
+        """"""
