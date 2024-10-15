48e3dbd2d32813b0dc96c9872b157ff0b66bc7ef
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-10-15 10:25:07 +0300

    Feat: Implemented the normalize_data method

--- scripts/data_cleaner.py
+++ scripts/data_cleaner.py
@@ -37,7 +37,7 @@ class Preprocessor:
     def remove_special_characters(text: str):
         """
         A function that will remove punctuations, this includes amharic punctuations list ፤, ። and ፣.
-    
+
         Args:
             text(str): the string from which the punctuations are going to be removed
         Returns:
@@ -48,7 +48,66 @@ class Preprocessor:
     
     @staticmethod
     def normalize_data(text: str):
-        """"""
+        """
+        A function that normalizes a submitted Amharic text. What is meant by normalizing is to replace 'Fidels' that have the same sound with one standard 'Fidel'.
+        The fidel chosen was at random, since the sounds are the same it doesn't matter which one is chosen.
+
+        Args: 
+            text(str): the Amharic text to be normalized
+        Returns:
+            The normalized string
+        """
+        rep1=re.sub('[ሃኅኃሐሓኻ]','ሀ',text)
+        rep2=re.sub('[ሑኁዅ]','ሁ',rep1)
+        rep3=re.sub('[ኂሒኺ]','ሂ',rep2)
+        rep4=re.sub('[ኌሔዄ]','ሄ',rep3)
+        rep5=re.sub('[ሕኅ]','ህ',rep4)
+        rep6=re.sub('[ኆሖኾ]','ሆ',rep5)
+        rep7=re.sub('[ሠ]','ሰ',rep6)
+        rep8=re.sub('[ሡ]','ሱ',rep7)
+        rep9=re.sub('[ሢ]','ሲ',rep8)
+        rep10=re.sub('[ሣ]','ሳ',rep9)
+        rep11=re.sub('[ሤ]','ሴ',rep10)
+        rep12=re.sub('[ሥ]','ስ',rep11)
+        rep13=re.sub('[ሦ]','ሶ',rep12)
+        rep14=re.sub('[ዓኣዐ]','አ',rep13)
+        rep15=re.sub('[ዑ]','ኡ',rep14)
+        rep16=re.sub('[ዒ]','ኢ',rep15)
+        rep17=re.sub('[ዔ]','ኤ',rep16)
+        rep18=re.sub('[ዕ]','እ',rep17)
+        rep19=re.sub('[ዖ]','ኦ',rep18)
+        rep20=re.sub('[ጸ]','ፀ',rep19)
+        rep21=re.sub('[ጹ]','ፁ',rep20)
+        rep22=re.sub('[ጺ]','ፂ',rep21)
+        rep23=re.sub('[ጻ]','ፃ',rep22)
+        rep24=re.sub('[ጼ]','ፄ',rep23)
+        rep25=re.sub('[ጽ]','ፅ',rep24)
+        rep26=re.sub('[ጾ]','ፆ',rep25)
+        #Normalizing words with Labialized Amharic characters such as በልቱዋል or  በልቱአል to  በልቷል  
+        rep27=re.sub('(ሉ[ዋአ])','ሏ',rep26)
+        rep28=re.sub('(ሙ[ዋአ])','ሟ',rep27)
+        rep29=re.sub('(ቱ[ዋአ])','ቷ',rep28)
+        rep30=re.sub('(ሩ[ዋአ])','ሯ',rep29)
+        rep31=re.sub('(ሱ[ዋአ])','ሷ',rep30)
+        rep32=re.sub('(ሹ[ዋአ])','ሿ',rep31)
+        rep33=re.sub('(ቁ[ዋአ])','ቋ',rep32)
+        rep34=re.sub('(ቡ[ዋአ])','ቧ',rep33)
+        rep35=re.sub('(ቹ[ዋአ])','ቿ',rep34)
+        rep36=re.sub('(ሁ[ዋአ])','ኋ',rep35)
+        rep37=re.sub('(ኑ[ዋአ])','ኗ',rep36)
+        rep38=re.sub('(ኙ[ዋአ])','ኟ',rep37)
+        rep39=re.sub('(ኩ[ዋአ])','ኳ',rep38)
+        rep40=re.sub('(ዙ[ዋአ])','ዟ',rep39)
+        rep41=re.sub('(ጉ[ዋአ])','ጓ',rep40)
+        rep42=re.sub('(ደ[ዋአ])','ዷ',rep41)
+        rep43=re.sub('(ጡ[ዋአ])','ጧ',rep42)
+        rep44=re.sub('(ጩ[ዋአ])','ጯ',rep43)
+        rep45=re.sub('(ጹ[ዋአ])','ጿ',rep44)
+        rep46=re.sub('(ፉ[ዋአ])','ፏ',rep45)
+        rep47=re.sub('[ቊ]','ቁ',rep46) #ቁ can be written as ቊ
+        rep48=re.sub('[ኵ]','ኩ',rep47) #ኩ can be also written as ኵ  
+
+        return rep48
     
     @staticmethod
     def remove_extra_space(text: str):
