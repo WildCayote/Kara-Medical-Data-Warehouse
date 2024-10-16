96a5a0481150f07acacaf8371bbd129aff4d8cdb
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-10-16 11:30:18 +0300

    Feat: Added phone number transformation

---
+++ kara_medical_project/models/phone_numbers_transformed.sql
@@ -0,0 +1,17 @@
+{{config(materialized='table')}}
+
+WITH message_pool AS (
+    SELECT 
+        id,
+        channel_id,
+        message
+    FROM {{source('public', 'channel')}} 
+)
+
+SELECT
+    id,
+    channel_id,
+    array_to_string(ARRAY(
+            SELECT regexp_replace(unnest(regexp_matches("message", '09\s*[0-9]{8}', 'g')), '\s+', '', 'g')
+        ), ', ') AS phone_numbers,
+FROM message_pool
